109d28ac32fc3e9d5cc8553419787d837c26205d
Rob Innes Hislop <rob.hislop@rd.io>
2015-04-18 23:58:33 -0700

    TableView setup

--- Stinky Tomatoes/Stinky Tomatoes/MoviesViewController.swift
+++ Stinky Tomatoes/Stinky Tomatoes/MoviesViewController.swift
@@ -8,19 +8,48 @@
 
 import UIKit
 
-class MoviesViewController: UIViewController {
-
+class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    var movies = [NSDictionary]?
+    
+    @IBOutlet var tableView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        let url = NSURL(string: "http://api.rottentomatoes.com/api/public/v1.0/lists/movies/box_office.json?apikey=dagqdghwaq3e3mxyrp7kmmj5&limit=20&country=US")!
+        let request = NSURLRequest(URL: url)
+        NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue()) { (
+            response: NSURLResponse!, data: NSData!, error: NSError!) -> Void in
+            let json = NSJSONSerialization.JSONObjectWithData(data, options: nil, error: nil) as? NSDictionary
+            
+            if let json = json {
+                self.movies = json["movies"] as! [NSDictionary]
+            }
+            
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        // Dispose of any resourcesthat can be recreated.
     }
     
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        // Row display. Implementers should *always* try to reuse cells by setting each cell's reuseIdentifier and querying for available reusable cells with dequeueReusableCellWithIdentifier:
+        // Cell gets various attributes set automatically based on table (separators) and data source (accessory views, editing controls)
+        
+        if let movies = movies {
+            return movies.count
+        } else {
+            return 0
+        }
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+    }
 
     /*
     // MARK: - Navigation
